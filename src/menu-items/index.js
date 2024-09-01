d04d3b0a37e92b75c427ce436154016ad0fa2531
spdev2023 <spdev2023@gmail.com>
2024-09-01 12:25:48 +0530

    first commit

---
+++ src/menu-items/index.js
@@ -0,0 +1,13 @@
+import dashboard from './dashboard';
+import other from './other';
+import utilities from './utilities';
+
+// ==============================|| MENU ITEMS ||============================== //
+
+const menuItems = {
+  items: [dashboard, utilities, other]
+
+  // items: [dashboard, pages, utilities, other]
+};
+
+export default menuItems;
